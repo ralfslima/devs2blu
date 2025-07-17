1d5e0b351c64408bb217a5c050f1f82912813308
ralfslima <62228067+ralfslima@users.noreply.github.com>
2025-07-17 19:58:20 -0300

    Arquivos da oitava aula do treinamento

--- Módulo 02 - HTML, CSS e JS/Aula 07/api/index.js
+++ Módulo 02 - HTML, CSS e JS/Aula 07/api/index.js
@@ -1,28 +1,23 @@
 // Importar dependência/pacote do Express
 const express = require('express');
 
+// Importar dependência/pacote do CORS
+const cors = require('cors');
+
 // Objeto Express (Manipular rotas e o servidor)
 const app = express();
+
+// Adicionar CORS
+app.use(cors());
 
 // Habilitar receber objeto JSON nas requisições
 app.use(express.json());
 
-// Vetor para armazenar dados pessoas
-let vetor = [
-    {id:1, nome:'Alice', idade:23},
-    {id:2, nome:'Caio',  idade:16}
-];
+// Referenciar arquivo de rotas
+const pessoa = require('./rotas/pessoa');
 
-// Rota para efetuar uma requisição GET
-app.get('/', (req, res) => {
-    //res.status(201).json({'nome':'Ralf', 'idade':35});
-    res.status(200).json(vetor);
-});
-
-// Rota para efetuar uma requisição POST
-app.post('/', (req, res) => {
-    res.send(req.body);
-});
+// Adicionar rotas na aplicação
+app.use('/', pessoa);
 
 // Servidor
 app.listen(3000);
