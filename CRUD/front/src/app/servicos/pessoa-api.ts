399a2044954e6998248ded627c14ea0653614d7b
ralfslima <62228067+ralfslima@users.noreply.github.com>
2025-08-28 16:50:08 -0300

    Add files via upload

---
+++ CRUD/front/src/app/servicos/pessoa-api.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pessoa } from '../modelo/Pessoa';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaApi {
+
+  // URL da API
+  private url:string = 'http://localhost:3000/pessoas/';
+
+  // Construtor
+  constructor(private http:HttpClient){}
+
+  // Listar todas as pessoas
+  listar():Observable<Pessoa[]>{
+    return this.http.get<Pessoa[]>(this.url);
+  }
+
+  // Cadastrar pessoas
+  cadastrar(pessoa:Pessoa):Observable<Pessoa>{
+    return this.http.post<Pessoa>(this.url, JSON.stringify(pessoa));
+  }
+
+  // Selecionar pessoa por id
+  selecionarPessoa(id:string):Observable<Pessoa>{
+    //return this.http.get<Pessoa>(this.url + id);
+    return this.http.get<Pessoa>(`${this.url}${id}`);
+  }
+
+}
+
+
+/*
+  RxJS:
+  - Observable: Executor
+  - Subscribe: Recebedor
+*/
